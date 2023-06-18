7ce7f22a0715cf29e6ff2086638bb64307be719c
patricio1984 <patricio1984@gmail.com>
2023-06-18 01:34:15 -0300

    uploading new burger menu and setting some classes fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,49 @@ import Header from './components/Header';
 import Map from './components/Map';
 import ISSDataProvider from './components/ISSDataProvider';
 import FooterData from './components/FooterData';
+import MenuBurger from './components/MenuBurger';
 import ISSFooterData from './components/ISSFooterData';
 import './App.css';
 
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <main className="main wrapper">
-        <section className="section">
-          <ISSDataProvider>
-            {({ isLoading, latitude, longitude, timestamp }) => (
-              <>
-                {isLoading ? (
-                  <div className="spinner__container">
-                      <div className="lds-roller" aria-label="Iss content Map is loading"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-                  </div>   
-                ) : (
-                  <>
-                    <Map latitude={latitude} longitude={longitude} />
-                    <FooterData
-                      latitude={latitude}
-                      longitude={longitude}
-                      timestamp={timestamp}
-                    />
-                  </>
-                )}
-              </>
-            )}
-          </ISSDataProvider>
-        </section>
-        <section className="ISSFooterData">
-          <ISSFooterData />
-        </section>
-      </main>
-    </>
+      <>
+        <MenuBurger />
+        
+        <Header />
+
+        <main className="main wrapper">
+          <section className="section">
+            <ISSDataProvider>
+              {({ isLoading, latitude, longitude, timestamp }) => (
+                <>
+                  {isLoading ? (
+                    <div className="spinner__container">
+                        <div className="lds-roller" aria-label="Iss content Map is loading"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+                    </div>   
+                  ) : (
+                    <>
+                      <Map latitude={latitude} longitude={longitude} />
+                      <FooterData
+                        latitude={latitude}
+                        longitude={longitude}
+                        timestamp={timestamp}
+                      />
+                    </>
+                  )}
+                </>
+              )}
+            </ISSDataProvider>
+          </section>
+
+          <section className="ISSFooterData">
+            <ISSFooterData />
+          </section>
+        </main>
+      </>
+
+
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { DarkModeToggle } from '@anatoliygatt/dark-mode-toggle';
+import React from 'react';
 
 const Header = () => {
-
-  const [mode, setMode] = useState('dark');
-
-  useEffect(() => {
-    if (mode !== 'dark') {
-      document.body.classList.add('light-mode');
-    } else {
-      document.body.classList.remove('light-mode');
-    }
-  }, [mode]);
-
   return (
     <header className="header">
         <div className="wrapper">
             <h1 className="title">See where is the ISS in real time</h1>
-
-            <DarkModeToggle
-                ariaLabel="Toggle color scheme"
-                mode={mode}
-                dark="Dark"
-                light="Light"
-                size="sm"
-                inactiveLabelColor="#CCCCCC"
-                activeLabelColor="#333333"
-                activeTrackColor="#e2e8f0"
-                activeTrackColorOnHover="#e2e8f0"
-                activeTrackColorOnActive="#cbd5e1"
-                inactiveTrackColor="#334155"
-                inactiveTrackColorOnHover="#1e293b"
-                inactiveTrackColorOnActive="#0f172a"
-                inactiveThumbColor="#e2e8f0" 
-                activeThumbColor="#1e293b"
-                onChange={(mode) => {
-                    setMode(mode);
-                }}
-            />
        </div>
     </header>
   );
